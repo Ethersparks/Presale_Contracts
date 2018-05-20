83c755f76debe2a25fd83c252a237d5b2bbea581
Dave <dsrour@eng.ucsd.edu>
2018-05-19 21:52:51 -0700

    - Started on loot crate generation contract

---
+++ PresaleLootCrateFactory.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.4.23;
+
+/**
+ * @title Ethersparks Presale Loot Crate Factory
+ * This contract defines a loot crate and is in charge of creating the loot crates during presale.
+ */
+ contract PresaleLootCrateFactory {
+     uint dnaDigits = 16; // should allow for sufficient variability during the presale creation of lootboxes
+     uint dnaMod = 10 ** dnaDigits;
+     
+     uint uniqueIdCounter = 0; // used during generation of loot crate
+     
+     struct LootCrate {
+         uint dna;
+     }
+     
+    LootCrate[] public lootCrates;
+    
+    function generateLootCrateDna(bool _isReferralLootCrate) private view returns (uint) {
+        /**
+         * This function generates a loot crate dna based on:
+         * - sender's address
+         * - uniqueIdCounter
+         * Since the meaning of the crate's dna won't be defined until after the presale,
+         * it is ok (and safe) to generate them from those inputs.
+         * A referral loot crate has its least significant bit toggled.
+         */
+         uint rand = uint(keccak256(abi.encodePacked(uint(msg.sender) + uniqueIdCounter)));
+         uint dna = rand % dnaMod;
+         if (_isReferralLootCrate)
+            dna = dna | 0x01;
+        else
+            dna = dna & 0x00;
+         return dna;
+    }
+    
+    
+    
+     
+ }
